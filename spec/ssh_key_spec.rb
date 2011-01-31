c9b5bfbfd6a12810bb02994c2361bfb92b69065e
Julian Bäume <julian@svg4all.de>
2011-01-31 02:18:52 +0100

    work on specification
    * move configs and keys into own directory, so it can be tested in a
    GitoliteAdmin context
    * use the let() method for specs for better maintainability

--- spec/ssh_key_spec.rb
+++ spec/ssh_key_spec.rb
@@ -2,51 +2,52 @@ require 'gitolite/ssh_key'
 include Gitolite
 
 describe Gitolite::SSHKey do
+  let(:key_dir) { File.join(File.dirname(__FILE__),'test-repo','keydir') }
   describe '#owner' do
     it 'owner should be bob for bob.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob.pub')
+      key = File.join(key_dir, 'bob.pub')
       s = SSHKey.new(key)
       s.owner.should == 'bob'
     end
 
     it 'owner should be bob for bob@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@desktop.pub')
+      key = File.join(key_dir, 'bob@desktop.pub')
       s = SSHKey.new(key)
       s.owner.should == 'bob'
     end
 
     it 'owner should be bob@zilla.com for bob@zilla.com.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@zilla.com.pub')
+      key = File.join(key_dir, 'bob@zilla.com.pub')
       s = SSHKey.new(key)
       s.owner.should == 'bob@zilla.com'
     end
 
     it 'owner should be bob@zilla.com for bob@zilla.com@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@zilla.com@desktop.pub')
+      key = File.join(key_dir, 'bob@zilla.com@desktop.pub')
       s = SSHKey.new(key)
       s.owner.should == 'bob@zilla.com'
     end
 
     it 'owner should be jakub123 for jakub123.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'jakub123.pub')
+      key = File.join(key_dir, 'jakub123.pub')
       s = SSHKey.new(key)
       s.owner.should == 'jakub123'
     end
 
     it 'owner should be jakub123@foo.net for jakub123@foo.net.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'jakub123@foo.net.pub')
+      key = File.join(key_dir, 'jakub123@foo.net.pub')
       s = SSHKey.new(key)
       s.owner.should == 'jakub123@foo.net'
     end
 
     it 'owner should be joe@sch.ool.edu for joe@sch.ool.edu' do
-      key = File.join(File.dirname(__FILE__),'keys', 'joe@sch.ool.edu.pub')
+      key = File.join(key_dir, 'joe@sch.ool.edu.pub')
       s = SSHKey.new(key)
       s.owner.should == 'joe@sch.ool.edu'
     end
 
     it 'owner should be joe@sch.ool.edu for joe@sch.ool.edu@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'joe@sch.ool.edu@desktop.pub')
+      key = File.join(key_dir, 'joe@sch.ool.edu@desktop.pub')
       s = SSHKey.new(key)
       s.owner.should == 'joe@sch.ool.edu'
     end
@@ -54,49 +55,49 @@ describe Gitolite::SSHKey do
 
   describe '#location' do
     it 'location should be "" for bob.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob.pub')
+      key = File.join(key_dir, 'bob.pub')
       s = SSHKey.new(key)
       s.location.should == ''
     end
 
     it 'location should be "desktop" for bob@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@desktop.pub')
+      key = File.join(key_dir, 'bob@desktop.pub')
       s = SSHKey.new(key)
       s.location.should == 'desktop'
     end
 
     it 'location should be "" for bob@zilla.com.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@zilla.com.pub')
+      key = File.join(key_dir, 'bob@zilla.com.pub')
       s = SSHKey.new(key)
       s.location.should == ''
     end
 
     it 'location should be "desktop" for bob@zilla.com@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob@zilla.com@desktop.pub')
+      key = File.join(key_dir, 'bob@zilla.com@desktop.pub')
       s = SSHKey.new(key)
       s.location.should == 'desktop'
     end
 
     it 'location should be "" for jakub123.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'jakub123.pub')
+      key = File.join(key_dir, 'jakub123.pub')
       s = SSHKey.new(key)
       s.location.should == ''
     end
 
     it 'location should be "" for jakub123@foo.net.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'jakub123@foo.net.pub')
+      key = File.join(key_dir, 'jakub123@foo.net.pub')
       s = SSHKey.new(key)
       s.location.should == ''
     end
 
     it 'location should be "" for joe@sch.ool.edu' do
-      key = File.join(File.dirname(__FILE__),'keys', 'joe@sch.ool.edu.pub')
+      key = File.join(key_dir, 'joe@sch.ool.edu.pub')
       s = SSHKey.new(key)
       s.location.should == ''
     end
 
     it 'location should be "desktop" for joe@sch.ool.edu@desktop.pub' do
-      key = File.join(File.dirname(__FILE__),'keys', 'joe@sch.ool.edu@desktop.pub')
+      key = File.join(key_dir, 'joe@sch.ool.edu@desktop.pub')
       s = SSHKey.new(key)
       s.location.should == 'desktop'
     end
@@ -104,7 +105,7 @@ describe Gitolite::SSHKey do
 
   describe '#keys' do
     it 'should load ssh key properly' do
-      key = File.join(File.dirname(__FILE__),'keys', 'bob.pub')
+      key = File.join(key_dir, 'bob.pub')
       s = SSHKey.new(key)
       parts = File.read(key).split #should get type, blob, email
 
@@ -116,7 +117,7 @@ describe Gitolite::SSHKey do
 
   describe '#email' do
     it 'should use owner if email is missing' do
-      key = File.join(File.dirname(__FILE__),'keys', 'jakub123@foo.net.pub')
+      key = File.join(key_dir, 'jakub123@foo.net.pub')
       s = SSHKey.new(key)
       s.owner.should == s.email
     end
